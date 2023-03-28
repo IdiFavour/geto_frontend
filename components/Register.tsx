788a5aba309ae2d38a4ed58c245cf345f280b25f
Idi Favour <idifavour7@gmail.com>
2023-03-28 16:34:18 +0100

    started registration page

---
+++ components/Register.tsx
@@ -0,0 +1,98 @@
+const Register = () => {
+  return (
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-6">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+            Sign Up for Geto
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="relative rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="block w-full rounded-md border-0 py-1.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Name"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium leading-6 text-gray-900 mt-2"
+              >
+                Email
+              </label>
+              <div className="relative rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="block w-full rounded-md border-0 py-1.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Name"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium leading-6 text-gray-900 mt-2"
+              >
+                Password
+              </label>
+              <div className="relative rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="block w-full rounded-md border-0 py-1.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Name"
+                />
+              </div>
+            </div>{" "}
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Creating an account means you’re okay with our Terms of Service,
+                Privacy Policy, and our default Notification Settings.
+              </label>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
